6a6cb75c1117af71ac5db2ae843351014f678fc2
genyaFiL <genyafil@gmail.com>
2023-02-27 23:08:28 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-document.body.classList.add("body-color");
-const bodyColor = document.querySelector(".body-color");
+
+const bodyColor = document.querySelector("body");
 const text = document.querySelector(".color");
 const btn = document.querySelector(".change-color");
 
-btn.addEventListener("click", (event) => {
+btn.addEventListener("click", () => {
   const hexColor = getRandomHexColor();
   text.textContent = hexColor;
   bodyColor.style.backgroundColor = hexColor;
